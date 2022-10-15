02c60f01b4799cc9162d17f6d7dc3a49e6e5138d
Septian Dika Maulana <C2318G2774@bangkit.academy>
2022-10-16 06:05:08 +0700

    .

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const path = require('path')
+const http = require('http')
+const socketio = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+io.on('connection', (socket) => {
+    console.log('New websocket connection')
+
+    socket.emit('message', 'Welcome!')
+
+    socket.on('SendMessage', (message) => {
+        io.emit('message', message)
+    })
+
+
+    // socket.emit('countUpdated', count)
+
+    // socket.on('increment', () => {
+    //     count++
+    //     // socket.emit('countUpdated', count)
+    //     io.emit('countUpdated', count)
+    // })
+})
+
+server.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
