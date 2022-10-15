e5b8e431e4fd6e0389ec4cd3989332f7579b16e6
Septian Dika Maulana <C2318G2774@bangkit.academy>
2022-10-16 06:10:24 +0700

    .

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,14 +8,6 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     e.preventDefault()
 
     const message = e.target.elements.message.value
+
     socket.emit('sendMessage', message)
 })
-
-// socket.on('countUpdated', (count) => {
-//     console.log('The count has been updated!', count)
-// })
-
-// document.querySelector('#increment').addEventListener('click', () => {
-//     console.log('Clicked')
-//     socket.emit('increment')
-// })
