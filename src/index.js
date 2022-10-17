e94074ba65548ac5c04c4f1239931c1c832ae6af
Septian Dika Maulana <C2318G2774@bangkit.academy>
2022-10-17 07:34:37 +0700

    .

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-const express = require('express')
 const path = require('path')
 const http = require('http')
+const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -13,24 +14,33 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
 io.on('connection', (socket) => {
-    console.log('New websocket connection')
+    console.log('New WebSocket connection')
 
     socket.emit('message', 'Welcome!')
+    socket.broadcast.emit('message', 'A new user has joined!')
 
-    socket.on('SendMessage', (message) => {
+    socket.on('sendMessage', (message, callback) => {
+
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed!')
+        }
+
         io.emit('message', message)
+        callback()
     })
 
+    socket.on('SendLocation', (coords) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        callback()
+    })
 
-    // socket.emit('countUpdated', count)
-
-    // socket.on('increment', () => {
-    //     count++
-    //     // socket.emit('countUpdated', count)
-    //     io.emit('countUpdated', count)
-    // })
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
+    })
 })
 
 server.listen(port, () => {
-    console.log('Server is up on port ' + port)
+    console.log(`Server is up on port ${port}!`)
 })
